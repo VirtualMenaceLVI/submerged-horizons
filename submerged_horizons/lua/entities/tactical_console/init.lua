152619871216c1b2806254331f3b91d95fd91305
Virtual Menace <menacestudios.buisness@gmail.com>
2026-04-09 15:30:13 -0400

    Change model for tactical console entity

--- submerged_horizons/lua/entities/tactical_console/init.lua
+++ submerged_horizons/lua/entities/tactical_console/init.lua
@@ -3,7 +3,7 @@ AddCSLuaFile("shared.lua")
 include("shared.lua")
 
 function ENT:Initialize()
-    self:SetModel("models/props_lab/reciever01b.mdl")
+    self:SetModel("models/azdesk/azdesk.mdl")
     self:PhysicsInit(SOLID_VPHYSICS)
     self:SetMoveType(MOVETYPE_VPHYSICS)
     self:SetSolid(SOLID_VPHYSICS)
